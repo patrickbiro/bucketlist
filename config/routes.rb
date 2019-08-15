454c52d10a268f2c4f78cb082cd742e25abe1d87
Patrick Biro <patrick.y.biro@gmail.com>
2019-08-15 22:40:38 +0200

    Add the controller ideas

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  get 'home', to:'home#index'
 
   get 'styles/atoms'
 
   get 'styles/molecules'
 
   get 'styles/organisms'
+
+  get 'ideas/index'
+
+  get 'styleguide', to:'styles#atoms'
+
+  get 'styleguide/form/example', to:'styles#atoms'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
