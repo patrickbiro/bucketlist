d1099fd50b14c919c8913592f5d267791b8ac452
Patrick Biro <patrick.y.biro@gmail.com>
2019-08-17 00:06:04 +0200

    Added a create function in controller. Creating an new Idea from the from ideas/new

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -2,10 +2,15 @@ class IdeasController < ApplicationController
   def index
     @search_term = params[:search_input]
     logger.info("The search term is #{@search_term}.")
+    @ideas = Idea.all
+    logger.info("The ideas existing are #{@ideas}.")
   end
   def new
   end
   def create
+    idea = Idea.new
+    idea.title = params[:title]
+    idea.save!
     redirect_to(ideas_index_path)
   end
 
