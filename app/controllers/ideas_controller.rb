23eb7813681701277549cdc690726e8b56e89770
Patrick Biro <patrick.y.biro@gmail.com>
2019-08-18 20:28:47 +0200

    Added edit page with all routes and controllers set

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -16,4 +16,24 @@ class IdeasController < ApplicationController
     redirect_to(account_ideas_path)
   end
 
+  def edit
+    id = params[:id]
+    @idea=Idea.find(id)
+  end
+
+  def update
+    # extract the identifier from the params Hash
+    id = params[:id]
+
+    # retrieve the instance using the identifier
+    idea = Idea.find(id)
+
+    idea.title = params[:title]
+    idea.done_count = params[:done_count]
+    idea.photo_url = params[:photo_url]
+    idea.save!
+    redirect_to(account_ideas_path)
+  end
+
+
 end
