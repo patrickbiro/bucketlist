7304b3e5f6f4d364d87dfaf6020641a7013ab178
Patrick Biro <patrick.y.biro@gmail.com>
2019-08-23 12:55:13 +0200

    added test files 2

--- test/system/ideas_test.rb
+++ test/system/ideas_test.rb
@@ -1,13 +1,13 @@
 require "application_system_test_case"
 
 class IdeasTest < ApplicationSystemTestCase
-  #test 'that Ideas are created correctly' do #1 asertions
-    #visit('/ideas/new')
-    #fill_in 'title', with: 'See the Matterhorn'
-    #click_on 'Create idea'
-    #visit('/account/ideas')
-    #assert page.has_content?('See the Matterhorn')
-  #end
+  test 'that Ideas are created correctly' do #1 asertions
+    visit('/ideas/new')
+    fill_in 'title', with: 'See the Matterhorn'
+    click_on 'Create idea'
+    visit('/account/ideas')
+    assert page.has_content?('See the Matterhorn')
+  end
 
   test 'that Ideas are loaded in the index' do #3 asertions
 
@@ -19,7 +19,7 @@ class IdeasTest < ApplicationSystemTestCase
     second_idea.title = 'See a giraffe in the wild'
     second_idea.save!
 
-    visit('/account/ideas')
+    visit('/ideas/index')
     assert page.has_content?('Cycle across Australia')
     assert page.has_content?('See a giraffe in the wild')
     assert_equal 2, page.all(class: 'card__thumb').count
