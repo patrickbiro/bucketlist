356f52fe50e44a4e32667f06062b8c4839d07c7f
Patrick Biro <patrick.y.biro@gmail.com>
2019-08-27 18:56:49 +0200

    Comment model tests on association with idea model

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -20,4 +20,35 @@ class CommentTest < ActiveSupport::TestCase
     comment.save!
     assert_equal(comment.idea.title, "Test2")
   end
+
+  test 'cascading save' do
+    Idea.all.delete_all
+    first_idea = Idea.new title:"Test"
+    first_idea.save!
+
+    comment = Comment.new
+    comment.body = "Great idea!"
+
+    first_idea.comments << comment
+    first_idea.save!
+
+    assert_equal(first_idea.comments.first.body, "Great idea!")
+  end
+
+  test 'Comments are ordered correctly' do
+    idea = Idea.new title:"Test"
+    idea.save!
+
+    comment1 = Comment.new body: "This would be great fun"
+    comment2 = Comment.new body: "I agree! I'd like to do this as well"
+
+    idea.comments << comment1
+    idea.comments << comment2
+
+    idea.save!
+
+    assert_equal(idea.comments.first, comment1)
+    assert_equal(idea.comments.length, 2)
+
+  end
 end
