1989d4c0957dace7688dcbddfb6348a9869ffcc1
Patrick Biro <patrick.y.biro@gmail.com>
2019-08-27 18:43:57 +0200

    COmment class model first test

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'changing the associated Idea for a Comment' do
+    Idea.all.delete_all
+    first_idea = Idea.new
+    first_idea.title="Test"
+    first_idea.save!
+
+    comment = Comment.new
+    comment.body = "I'd like to do this!"
+
+
+    second_idea = Idea.new
+    second_idea.title="Test2"
+    second_idea.save!
+
+    comment.idea = second_idea
+    comment.save!
+    assert_equal(comment.idea.title, "Test2")
+  end
 end
