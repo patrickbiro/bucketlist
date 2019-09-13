b100c4c26cd7b539ff05344366087e19c6b72f21
Patrick Biro <patrick.y.biro@gmail.com>
2019-09-13 14:39:32 +0200

    Added password attribute to user class and has_secure_password in class

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190829084937) do
+ActiveRecord::Schema.define(version: 20190913123801) do
 
   create_table "comments", force: :cascade do |t|
     t.string "body"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20190829084937) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
