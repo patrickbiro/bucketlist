11102668f35519f0f3153b4657699a4908bc042e
Patrick Biro <patrick.y.biro@gmail.com>
2019-09-14 20:35:11 +0200

    Implement filter on the account page. Only logged users can access their own ideas

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -1,6 +1,18 @@
 class AccountController < ApplicationController
+  before_action :ensure_authenticated
+
   def ideas
-    @ideas = Idea.all
+    user_id = session[:user_id]
+    user = User.find(user_id)
+    @ideas = user.ideas
+
     logger.info("The ideas existing are #{@ideas}.")
   end
+
+  def ensure_authenticated
+    unless(logged_in?)
+      redirect_to new_session_path
+    end
+  end
+
 end
