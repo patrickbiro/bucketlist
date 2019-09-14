32b2672b0006c33ac963e94e59ee90e82563e942
Patrick Biro <patrick.y.biro@gmail.com>
2019-09-14 17:51:37 +0200

    Finalize separate login and signup page with adequate controllers and views

--- test/system/log_ins_test.rb
+++ test/system/log_ins_test.rb
@@ -5,19 +5,21 @@ class LogInsTest < ApplicationSystemTestCase
     User.all.delete_all
     visit(new_user_path)
     fill_in 'Email address', with: 'patrick@epfl.ch'
-    click_on 'Log in'
+    fill_in 'Password', with: 'password'
+    click_on 'Sign in'
     assert(User.all.length>0)
     assert_equal(User.first.email, 'patrick@epfl.ch')
   end
 
   test 'log in does not create a User' do #1 asertions
     User.all.delete_all
-    user= User.new email: 'patrick@epfl.ch'
-    visit(new_user_path)
+    user= User.new email: 'patrick@epfl.ch', password: 'password'
+    visit(new_session_path)
     fill_in 'Email address', with: 'patrick@epfl.ch'
+    fill_in 'Password', with: 'password'
     click_on 'Log in'
     assert(User.all.length==1)
   end
 
-  
+
 end
