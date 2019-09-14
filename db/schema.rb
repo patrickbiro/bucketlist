641e34f86db08c776e6024fce9c384d5caa42f35
Patrick Biro <patrick.y.biro@gmail.com>
2019-09-14 20:38:28 +0200

    Added avatar_url attibute to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190913123801) do
+ActiveRecord::Schema.define(version: 20190914183746) do
 
   create_table "comments", force: :cascade do |t|
     t.string "body"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20190913123801) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "avatar_url"
   end
 
 end
