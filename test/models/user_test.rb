62f8be70fc452452e431739274176b447de19e0b
Patrick Biro <patrick.y.biro@gmail.com>
2019-09-14 19:04:56 +0200

    Added email downcase validation callback and during login

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'test that user creation is test insensitive' do
+    user= User.new email: 'nEw@EpFl.Ch', password: 'password'
+    user.save!
+    assert user.valid?
+    assert_equal(user.email, 'new@epfl.ch')
+  end
 end
