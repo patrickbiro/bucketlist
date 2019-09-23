4873c3d62e3106c42949585e9427ed93d4b35767
Patrick Biro <patrick.y.biro@gmail.com>
2019-09-23 18:42:09 +0200

    Added asychronous type action to add goal in cards. Also modified goal controller and added a partial create.js.erb to goal

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,10 +1,13 @@
 class GoalsController < ApplicationController
-    before_action :ensure_authenticated
+  before_action :ensure_authenticated
 
   def create
-    user = current_user
-    idea = Idea.find(params[:idea_id])
-    user.goals << idea
-    redirect_to(idea_path(idea))
+    @user = current_user
+    @idea = Idea.find(params[:idea_id])
+    @user.goals << @idea
+    respond_to do |format|
+      format.html { redirect_to(idea_path(idea)) }
+      format.js
+    end
   end
 end
