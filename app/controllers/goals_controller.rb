803fcf26fb30080ce87e29b499d3d047586d927b
Patrick Biro <patrick.y.biro@gmail.com>
2019-09-23 21:07:53 +0200

    Refactored javascript (bulk js in asset)

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -4,9 +4,10 @@ class GoalsController < ApplicationController
   def create
     @user = current_user
     @idea = Idea.find(params[:idea_id])
+    logger.warn('User and idea exist!')
     @user.goals << @idea
     respond_to do |format|
-      format.html { redirect_to(idea_path(idea)) }
+      format.html { redirect_to(idea_path(@idea)) }
       format.js
     end
   end
