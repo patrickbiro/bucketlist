7b79ab2375108f7ae8d32a23e2b5969797072872
Patrick Biro <patrick.y.biro@gmail.com>
2019-09-26 15:53:10 +0200

    Added unsplash gem in gemfile and config file under config/initializer

---
+++ config/initializers/unsplash.rb
@@ -0,0 +1,4 @@
+Unsplash.configure do |config|
+  config.application_access_key = '0da4785a86ab899079cf444f655d98d6b7deb994dbf2f1b3ef131f35f407af9f'
+  config.utm_source             = "exts-fullstack"
+end
