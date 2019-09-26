ef7a31cd42d3e03945d1ffe7ce0d8cc017182173
Patrick Biro <patrick.y.biro@gmail.com>
2019-09-26 14:36:46 +0200

    Added remove goal button

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -4,11 +4,22 @@ class GoalsController < ApplicationController
   def create
     @user = current_user
     @idea = Idea.find(params[:idea_id])
-    logger.warn('User and idea exist!')
+    logger.info('User and idea exist!')
     @user.goals << @idea
     respond_to do |format|
       format.html { redirect_to(idea_path(@idea)) }
       format.js
     end
+  end
+
+  def destroy
+    user = current_user
+    logger.info("USER ID #{user.id}")
+    idea = Idea.find(params[:id])
+    logger.info("IDEA ID #{idea.id}")
+    user.goals.destroy idea
+    redirect_to(account_goals_path)
+
+
   end
 end
