68eff7b30c1b6e5d539df59a303acc2401cd0ec2
Patrick Biro <patrick.y.biro@gmail.com>
2019-10-18 18:24:33 +0200

    Fixed test using selenium

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,6 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :rack_test
+  #driven_by :rack_test
+  driven_by :selenium, using: :firefox, screen_size: [1200, 1200]
 end
---
+++ test/system/using_capybara_test.rb
@@ -0,0 +1,17 @@
+require 'application_system_test_case'
+
+Capybara.run_server = false
+
+class CapybaraTest < ApplicationSystemTestCase
+  #test 'capybara works' do
+    #visit('http://www.google.com')
+    #sleep(5.seconds)
+    #fill_in('q', with: 'Ruby on Rails')
+    #sleep(5.seconds)
+    #click_on('Google-Suche', match: :first)
+    #click_on('Ruby on Rails', match: :first)
+    #sleep(5.seconds)
+    #click_on('Guides')
+    #sleep(20.seconds)
+  #end
+end
